46e45298b272d426bfdbc4c67a6ed60629f999f1
GabrielMTF <gabrielmota287@gmail.com>
2022-08-24 09:28:16 -0300

    início do desenvolvimento

--- financy/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Financycontrol from './Financycontrol'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Financycontrol />
-  </React.StrictMode>
-)
