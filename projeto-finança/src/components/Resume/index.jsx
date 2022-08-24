7d2b99e113031edf52c9a7c5efd546945542865e
GabrielMTF <gabrielmota287@gmail.com>
2022-08-24 11:40:31 -0300

    início do desenvolvimento

--- projeto-finança/src/components/Resume/index.jsx
+++ projeto-finança/src/components/Resume/index.jsx
@@ -3,12 +3,12 @@ import * as C from './styles';
 import ResumeItem from '../ResumeItem'
 import { FaRegArrowAltCircleUp, FaRegArrowAltCircleDown, FaDollarSign } from 'react-icons/fa'
 
-const Resume = () => {
+const Resume = ({ income, expense, total }) => {
     return (
         <C.Container>
-            <ResumeItem color='green' Title="Entradas" Icon={FaRegArrowAltCircleUp} value='5000'/>
-            <ResumeItem color='red' Title="Saída" Icon={FaRegArrowAltCircleDown} value='600'/>
-            <ResumeItem color='gold' Title="Total" Icon={FaDollarSign} value='4400'/>
+            <ResumeItem color='green' Title="Entradas" Icon={FaRegArrowAltCircleUp} value={income}/>
+            <ResumeItem color='red' Title="Saída" Icon={FaRegArrowAltCircleDown} value={expense}/>
+            <ResumeItem color='gold' Title="Total" Icon={FaDollarSign} value={total} />
         </C.Container>
     )
 }
